8952ad83c2dc84201c8496089c72a1bc5baa8203
paweloczkowski <paweloczkowski@github.com>
2017-05-06 18:48:24 +0200

    modified file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+    def current_user_is_admin
+        current_user&& current_user.is_admin?
+    end
+
+    def login_or_logout_user_link
+      current_user ? logout_user_link : login_user_link
+    end
+
+    def login_user_link
+      link_to 'Login', new_user_session_path
+    end
+
+    def logout_user_link
+      link_to 'Logout', destroy_user_session_path, method: :delete
+    end
 end
