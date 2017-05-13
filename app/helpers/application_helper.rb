3c0abbe62a27034f93380290332bf3b0521df2ec
paweloczkowski <paweloczkowski@github.com>
2017-05-13 12:38:34 +0200

    refactor current user is admin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,4 @@
 module ApplicationHelper
-    def current_user_is_admin
-        current_user && current_user.is_admin?
-    end
-
     def login_or_logout_user_link
       current_user ? logout_user_link : login_user_link
     end
@@ -17,6 +13,6 @@ module ApplicationHelper
 
     def admin_categories_link
       categories_link_tag = "<li>#{link_to 'Categories', categories_path}</li>"
-      categories_link_tag.html_safe if current_user_is_admin
+      categories_link_tag.html_safe if current_user.is_admin?
     end
 end
