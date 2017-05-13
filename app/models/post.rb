b60f6ef8598d3f4ae4dceba8118d781c99032318
paweloczkowski <paweloczkowski@github.com>
2017-05-13 12:59:42 +0200

    add validation for date for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :user
   validates :title, :expiration_date,  presence: true
+  validate :expiration_date_cannot_be_in_the_past
+
+  def expiration_date_cannot_be_in_the_past
+
+    if expiration_date < Date.today
+      errors.add(:expiration_date, "cannot be in the past")
+    end
+  end
 end
