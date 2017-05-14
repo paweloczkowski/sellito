0cd3f34ef3b29e75ba31e966137807599b155684
paweloczkowski <paweloczkowski@github.com>
2017-05-14 14:54:36 +0200

    changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 module ApplicationHelper
+  def current_user_is_admin
+    current_user&.is_admin?
+  end
+
+  def admin_categories_link
+    categories_link_tag = "<li>#{link_to 'Categories', categories_path}</li>"
+    categories_link_tag.html_safe if current_user.is_admin?
+  end
+
   def login_or_logout_user_link
     current_user ? logout_user_link : login_user_link
   end
@@ -9,10 +18,5 @@ module ApplicationHelper
 
   def logout_user_link
     link_to 'Logout', destroy_user_session_path, method: :delete
-  end
-
-  def admin_categories_link
-    categories_link_tag = "<li>#{link_to 'Categories', categories_path}</li>"
-    categories_link_tag.html_safe if current_user.is_admin?
   end
 end
