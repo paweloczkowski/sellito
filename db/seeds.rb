d1d7ae50e76c4b468bb252509d0925549ab3de98
paweloczkowski <paweloczkowski@github.com>
2017-05-14 14:47:54 +0200

    create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+admin = User.create(email: 'user@user.pl', password: 'password', is_admin?: true)
+user = User.create(email: 'klient@sellito.pl', password: 'password')
+
+categories= %w(home fashion garden beauty kids edu food moto it health hobby sport pets music)
+
+categories.each { |category| Category.create(name: category) }
+
+admin_post_1 = admin.posts.create(title: "Admin Post1", expiration_date: Time.now + 2.weeks)
+admin_post_2 = admin.posts.create(title: "Admin Post2", expiration_date: Time.now + 3.weeks)
+admin_post_3 = admin.posts.create(title: "Admin Post3", expiration_date: Time.now + 4.weeks)
+
+user_post_1 = user.posts.create(title: "User Post1", expiration_date: Time.now + 2.weeks)
+user_post_2 = user.posts.create(title: "User Post2", expiration_date: Time.now + 3.weeks)
+user_post_3 = user.posts.create(title: "User Post3", expiration_date: Time.now + 4.weeks)
+
+admin_post_1.categories << Category.first
+admin_post_1.categories << Category.last
+admin_post_2.categories << Category.first
+admin_post_2.categories << Category.last
+admin_post_2.categories << Category.find_by(name: 'edu')
+admin_post_3.categories << Category.first
+admin_post_3.categories << Category.last
+admin_post_3.categories << Category.find_by(name: 'edu')
+
+user_post_1.categories << Category.find_by(name: 'edu')
+user_post_1.categories << Category.first
+user_post_2.categories << Category.first
+user_post_2.categories << Category.first
+user_post_2.categories << Category.find_by(name: 'kids')
+user_post_3.categories << Category.first
+user_post_3.categories << Category.first
+user_post_3.categories << Category.first
